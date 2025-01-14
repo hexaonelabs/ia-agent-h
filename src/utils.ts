f8c8d350e7691101f4e58176f79f5b8d065c06c1
FazioNico <contact@nicolasfazio.ch>
2025-01-14 14:58:24 +0100

    refactor: rmv unused utils

--- src/utils.ts
+++ src/utils.ts
@@ -3,41 +3,6 @@ import * as yaml from 'js-yaml';
 import * as p from 'path';
 import { ToolConfig, yamlToToolParser } from './tools';
 
-export const upsertAgent = (
-  filePath: string,
-  agentName: string,
-  enabled: boolean,
-  additionalProperties: { [key: string]: any } = {},
-) => {
-  // Read the YAML file
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const data = yaml.load(fileContents);
-  // Find the agent by its name
-  const agent = data.Agents_list.find((agent: any) => agent.Name === agentName);
-  if (agent) {
-    // Update the `enabled` value
-    agent.enabled = enabled;
-  } else {
-    // Add the new agent to the list
-    const newAgent = { Name: agentName, enabled, ...additionalProperties };
-    data.Agents_list.push(newAgent);
-  }
-  // Convert the data to YAML
-  const newYaml = yaml.dump(data, { indent: 2 });
-  // Write the modified data back to the YAML file
-  fs.writeFileSync(filePath, newYaml, 'utf8');
-};
-
-export const getAgentsEnabled = (
-  filePath: string,
-): { name: string; enabled: true }[] => {
-  // Read the YAML file
-  const fileContents = fs.readFileSync(filePath, 'utf8');
-  const data = yaml.load(fileContents);
-  // Filter the agents by their `enabled` value
-  return data.Agents_list.filter((agent: any) => agent.enabled);
-};
-
 // replace `_` & `-` by convert to camel case
 export const toCamelCase = (value: string) => {
   return value.replace(/[-_](.)/g, (_, group) => group.toUpperCase());
