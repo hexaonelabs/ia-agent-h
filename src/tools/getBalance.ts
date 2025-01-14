bc3ededb30c8fa26f59db19998209891c67b548e
FazioNico <contact@nicolasfazio.ch>
2025-01-14 17:11:19 +0100

    fix: use args as object

--- src/tools/getBalance.ts
+++ src/tools/getBalance.ts
@@ -3,7 +3,12 @@ import { createViemPublicClient } from '../viem/createViemPublicClient';
 import { formatEther } from 'viem';
 import { mainnet, sepolia } from 'viem/chains';
 
-export async function getBalance(wallet: Address, network: string) {
+interface GetBalanceArgs {
+  wallet: Address;
+  network: string;
+}
+
+export async function getBalance({ wallet, network }: GetBalanceArgs) {
   const chain = getNetworkByName(network);
   const publicClient = createViemPublicClient(chain);
   const balance = await publicClient.getBalance({ address: wallet });
