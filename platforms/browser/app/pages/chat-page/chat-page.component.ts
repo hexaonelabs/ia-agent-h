fed841bb2fcd4e88ee7fa0833628802de2356447
FazioNico <contact@nicolasfazio.ch>
2025-01-21 18:53:44 +0100

    feat(browser): add sse mesaging

--- platforms/browser/app/pages/chat-page/chat-page.component.ts
+++ platforms/browser/app/pages/chat-page/chat-page.component.ts
@@ -5,6 +5,7 @@ import {
   afterNextRender,
   inject,
   PLATFORM_ID,
+  OnDestroy,
 } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import {
@@ -32,9 +33,10 @@ import {
 import { navigateOutline, powerOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { AppService } from '../../app.service';
-import { Observable, tap } from 'rxjs';
+import { filter, map, Observable, Subscription, tap } from 'rxjs';
 import { ConnectUserModalComponent } from '../../components/connect-user-modal/connect-user-modal.component';
 import { SafeHtmlPipe } from '../safe-html.pipe';
+import { SseService } from '../../sse.service';
 
 const UIElements = [
   IonApp,
@@ -57,17 +59,51 @@ const UIElements = [
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss',
 })
-export class ChatPageComponent implements AfterViewInit {
+export class ChatPageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('content') private content!: IonContent;
   private readonly _platform = inject(PLATFORM_ID);
   private readonly _document = inject(DOCUMENT);
+  private readonly _subscriptions: Subscription[] = [];
   public readonly userAccount$: Observable<`0x${string}` | undefined>;
+  /**
+   * Boolean indicating whether a response is pending from the server.
+   * Is use to display a loading dots animation & disable the input.
+   */
   public isPendingResponse: boolean = false;
+
+  /**
+   * Form control for the message input.
+   */
+  public readonly messageInput = new FormControl('');
+
+  /**
+   * Array of messages to display in the chat.
+   */
+  public readonly messages: Array<{
+    text: string;
+    type: 'user' | 'assistant';
+  }> = [];
+  currentThreadId?: string;
 
   constructor(
     private readonly _appService: AppService,
+    private readonly _sseService: SseService,
     private readonly _modalCtrl: ModalController,
   ) {
+    const sub = this._sseService
+      .createEventSource()
+      .pipe(
+        map(({ chat }) => chat),
+        filter((chat) => !!chat),
+      )
+      .subscribe((data) => {
+        this.messages.push({
+          text: data.message,
+          type: 'assistant',
+        });
+        this.scrollToBottom();
+      });
+    this._subscriptions.push(sub);
     this.userAccount$ = this._appService.account$.pipe(
       tap(async (account) => {
         const existingModal = await this._modalCtrl.getTop();
@@ -113,17 +149,13 @@ export class ChatPageComponent implements AfterViewInit {
   }
 
   /**
-   * Form control for the message input.
+   * Unsubscribe from all subscriptions when the component is destroyed.
    */
-  public readonly messageInput = new FormControl('');
-  /**
-   * Array of messages to display in the chat.
-   */
-  public readonly messages: Array<{
-    text: string;
-    type: 'user' | 'assistant';
-  }> = [];
-  currentThreadId?: string;
+  ngOnDestroy() {
+    // close ssl connection
+    this._sseService.close();
+    this._subscriptions.forEach((sub) => sub.unsubscribe());
+  }
 
   /**
    * Scroll to the bottom of the messages container.
---
+++ platforms/browser/app/sse.service.ts
@@ -0,0 +1,43 @@
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { Injectable } from '@angular/core';
+
+export interface MessageData {
+  chat?: {
+    threadId: string;
+    message: string;
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SseService {
+  private _eventSource: EventSource | undefined;
+  constructor() {}
+
+  createEventSource(): Observable<MessageData> {
+    if (this._eventSource) {
+      this._eventSource.close();
+    }
+    this._eventSource = new EventSource(environment.apiEndpoint + '/sse');
+    return new Observable((observer) => {
+      if (!this._eventSource) {
+        throw new Error('EventSource is not initialized');
+      }
+      this._eventSource.onmessage = (event) => {
+        const messageData: MessageData = JSON.parse(event.data);
+        console.log('Received message from sse: ', messageData);
+        observer.next(messageData);
+      };
+    });
+  }
+
+  close() {
+    if (!this._eventSource) {
+      return;
+    }
+    this._eventSource.close();
+    this._eventSource = undefined;
+  }
+}
